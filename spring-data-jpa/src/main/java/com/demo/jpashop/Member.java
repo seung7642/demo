7ebdd1417e2d2e2a013e162258ff2ff237a95d13
Seungho Lee <seung7642@gmail.com>
2021-12-05 16:12:06 +0900

    Spring-Data-JPA 데모

---
+++ spring-data-jpa/src/main/java/com/demo/jpashop/Member.java
@@ -0,0 +1,17 @@
+package com.demo.jpashop;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Getter @Setter
+@Entity
+public class Member {
+
+    @Id @GeneratedValue
+    private Long id;
+    private String username;
+}
