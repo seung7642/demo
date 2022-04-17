b2d79b9407ec13ee651cfbfb07766d803e65abce
이승호 <seung7642@gmail.com>
2022-04-17 18:19:16 +0900

    servlet (프론트 컨트롤러 패턴, 어댑터 패턴 적용), spring mvc 도입

---
+++ servlet/src/main/java/me/servlet/web/springmvc/old/OldController.java
@@ -0,0 +1,19 @@
+package me.servlet.web.springmvc.old;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+// 스프링 빈의 이름을 url-pattern과 맞춰서 명시하면, 해당 url로 요청이 왔을 때 알아서 매핑을 해줍니다.
+@Component("/springmvc/old-controller")
+public class OldController implements Controller {
+
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        System.out.println("OldController.handleRequest");
+        return new ModelAndView("new-form");
+    }
+}
